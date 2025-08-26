8b9df26f3df4293371ac661dfc7023a76ec222aa
alonecoder-dev <teja.vallabhadasu@gmail.com>
2025-08-26 20:15:57 +0530

    feat: Implement dynamic UI and configure Firebase
    - Set up Firebase project and Firestore database.
    - Install and configure Firebase SDK using secure environment variables.

---
+++ lestart/src/firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_apikey,
+  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+  projectId: import.meta.env.VITE_FIREBASE_projectId,
+  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_appId,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+export { db };
